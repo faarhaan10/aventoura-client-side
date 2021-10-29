edc771f2824fd4cdbda173ec2a3175df285261e2
Farhan Elias <faarhaan10@gmail.com>
2021-10-30 00:20:38 +0600

    order placment committed

--- src/componants/Placeorder/Placeorder.js
+++ src/componants/Placeorder/Placeorder.js
@@ -1,12 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row , Col, Image, Form, FloatingLabel,Button} from "react-bootstrap";
 import { useParams } from "react-router";
+import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
 
 const Placeorder = () => {
   const [plan,setPlan] = useState({});
+  const { register, handleSubmit } = useForm();
+  const {user} = useAuth()
   const { id } = useParams();
+
   const {image,tourTitile,description,duration,price,location} = plan;
-  console.log(plan)
+const planId = id;
+const status = 'pending';
+
+  const onSubmit = data => {
+    const newData = {planId,status,...data};
+    console.log(newData);
+
+    fetch('http://localhost:5000/tourists',{
+      method:'POST',
+      headers:{'content-type':'application.json'},
+      body: JSON.stringify(newData)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  };
+
+
   // load single plan 
   useEffect( () => {
     fetch(`http://localhost:5000/plans/${id}`)
@@ -33,28 +56,41 @@ const Placeorder = () => {
             <h4>Total cost: <span className="text-warning">${price}/Pack</span></h4>
           </Col>
         </Row>
-        <Form>
+
+{/* order placement form  */}
+        {tourTitile && <Form onSubmit={handleSubmit(onSubmit)}>
         <Row className='align-items-center py-3'>
-          <Col xs={12} md={6}>
+  {/* mailing information */}
+          <Col xs={12} md={6} className="px-5">
             <FloatingLabel label="Full Name" className="mb-3" >
-                <Form.Control type="text" placeholder="name" />
+                <Form.Control type="text" placeholder="name" 
+                defaultValue={user.displayName}
+                {...register("name", { required: true })}/>
             </FloatingLabel>
             <FloatingLabel label="Email" className="mb-3" >
-                <Form.Control type="email" placeholder="email" />
+                <Form.Control type="email" placeholder="email" 
+                defaultValue={user.email}
+                {...register("email", { required: true })}/>
             </FloatingLabel>
           </Col>
-          <Col xs={12} md={6}>
+
+  {/* tour plan information */}
+          <Col xs={12} md={6} className="px-5">
           <FloatingLabel label="Tour Package" className="mb-3" >
-                <Form.Control type="text" placeholder="plan" />
+                <Form.Control type="text" placeholder="plan" 
+                defaultValue={tourTitile}
+                {...register("plan", { required: true } )}/>
             </FloatingLabel>
             <FloatingLabel label="Date" className="mb-3" >
-                <Form.Control type="date" placeholder="date" />
+                <Form.Control type="date" placeholder="date"
+                 {...register("date", { required: true })}/>
             </FloatingLabel>
           </Col>
+
           <Button variant="warning" type='submit' className="w-25 mx-auto text-white px-5 py-3 fs-5 fw-bold"> Book now
           </Button>
         </Row>
-        </Form>
+        </Form>}
       </Container>
     </div>
   );
